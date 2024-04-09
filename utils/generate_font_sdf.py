39e45d65c01025c09d7a384b0a932e1491a79bde
Nick <nlharring@gmail.com>
2024-04-09 13:27:46 -0700

    Oh ha!

--- utils/generate_font_sdf.py
+++ utils/generate_font_sdf.py
@@ -1,1 +1,16 @@
 import json
+
+
+font_path = '/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf'
+
+import freetype
+face = freetype.Face(font_path)
+face.set_char_size( 48*64 )
+face.load_char('S')
+bitmap = face.glyph.bitmap
+print(bitmap.buffer)
+
+
+# Ok, what do we need to render fonts:
+# Need an SDF of the glyphs
+#
